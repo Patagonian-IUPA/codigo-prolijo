3a9de3f93d6565a20335bb514ac65ef0ff7dfd45
Arcaytus <68873171+Arcaytus@users.noreply.github.com>
2020-12-14 17:08:00 -0300

    Update class.js

--- class.js
+++ class.js
@@ -2,14 +2,14 @@ class Foo {
   color = 'negro';
   peso = 34;
   ver() {
-    console.info (`Color: ${this.color}. Peso: ${this.peso}`);
+    console.info(`Color: ${this.color}. Peso: ${this.peso}`);
   }
 }    
 
 class Bar {
   miMetodo() {
     if (this.imprimir) {
-      console.info ('Imprimir!');
+      console.info('Imprimir!');
     }
   } 
 }
