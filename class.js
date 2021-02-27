ae24c7e12152d1a4b8a303426339ef79c2ea5b5c
Gabriel Raby <grabyc@gmail.com>
2021-02-27 08:13:31 -0300

    PW1_TP_CodigoProlijo - Resuelto OK

--- class.js
+++ class.js
@@ -1,20 +1,18 @@
-class Foo
-{
-color='negro';peso   =    34;
-  ver()
-  {
-    console.info(`Color: ${this.color}. Peso: ${this.peso}`
-    );
-// foo = 123;
-/*
-// Prueba
-test() {
-  console.info('Test');
-}
-*/
-}
-   }    class Bar   {
-  miMetodo(){if(this.imprimir){console.info('Imprimir!');}}
+class Foo {
+  color = "negro";
+  peso = 34;
+
+  ver() {
+    console.info(`Color: ${this.color}. Peso: ${this.peso}`);
+  }
 }
 
-// Agregar property "imprimir=true" para que "miMetodo()" se ejecute.
+class Bar {
+  imprimir = true;
+
+  miMetodo() {
+    if (this.imprimir) {
+      console.info("Imprimir!");
+    }
+  }
+}
