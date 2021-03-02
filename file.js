05d304566661825a7ff0c9a0785506888904ddf5
gdsasso <gus_sasso@hotmail.com>
2021-03-02 09:45:07 -0300

    Update file.js
    Corregido

--- file.js
+++ file.js
@@ -1,80 +1,81 @@
 const db = new BaseDatos();
 // para prueba de no conectado
-if (new Date().getTime()%2) {
-  db.connect();
+if (new Date().getTime() % 2) {
+  db.connect ();
 }
 
-function leerDesdeDB() {
+function leerDesdeDB () {
   try {
-    Utils.disableAll();
-    const id = prompt('ID a leer:');
-    const data = db.read(id);
-    Utils.enableAll();
+    Utils.disableAll ();
+    const id = prompt ('ID a leer:');
+    const data = db.read (id);
+    Utils.enableAll ();
     return data;
-  } catch(err) {
-    alert(`Error- Vuelva a  ingresar el ID a leer!\n\n\t${err.message}`);
-    console.warn('Error- Vuelva a  ingresar el ID a leer',err);
-    //se vuelve a habilitar el boton
+  } 
+  catch (err) {
+    alert (`Error- Vuelva a  ingresar el ID a leer!\n\n\t${err.message}`);
+    console.warn ('Error- Vuelva a  ingresar el ID a leer', err);
+    // se vuelve a habilitar el boton
     return null;
   }
   finally {
-  Utils.enableAll();
+    Utils.enableAll ();
   }
 }
 
-function btnLeer() {
+function btnLeer () {
   try {
-    const data = leerDesdeDB();
-    console.info('Resultado:', data);
+    const data = leerDesdeDB ();
+    console.info ('Resultado:', data);
   }
-  catch(err) {
-    alert('Error al leer desde DB');
-    alert(`Error al leer desde DB\n\n\t${err.message}`);
-    console.warn('Error al leer desde DB',err);
+  catch (err) {
+    alert ('Error al leer desde DB');
+    alert (`Error al leer desde DB\n\n\t${err.message}`);
+    console.warn ('Error al leer desde DB', err);
   }
 }
 
-function btnConectar() {
+function btnConectar () {
   try {
-    Utils.disableAll();
-    db.connect();
+    Utils.disableAll ();
+    db.connect ();
   }
-  catch(err) {
-    alert('Error al conectar');
-    alert(`Error al conectar\n\n\t${err.message}`);
-    console.warn('Error al conectar',err);
+  catch (err) {
+    alert ('Error al conectar');
+    alert (`Error al conectar\n\n\t${err.message}`);
+    console.warn ('Error al conectar', err);
   }
   finally {
-    Utils.enableAll();
+    Utils.enableAll ();
   }
 }
 
-function btnCargar() {
+function btnCargar () {
   try {
-    Uils.disableAll();
-    const id = prompt('Nuevo ID:');
-    const dato = prompt('Nuevo Dato:');
-    db.save(id, dato);
+    Uils.disableAll ();
+    const id = prompt ('Nuevo ID:');
+    const dato = prompt ('Nuevo Dato:');
+    db.save (id, dato);
   }
-  catch(err) {
-    alert(`Error al cargar!\n\n\t${err.message}`)
-    console.warn('Error al cargar',err);
+  catch (err) {
+    alert (`Error al cargar!\n\n\t${err.message}`)
+    console.warn ('Error al cargar', err);
   } 
   finally {
-    Utils.enableAll();
+    Utils.enableAll ();
   }
 }
 
-function btnDesconectar() {
-  try{
-    Utils.disableAll();
-    db.close();
+function btnDesconectar () {
+  try {
+    Utils.disableAll ();
+    db.close ();
   }
-  catch(err) {
-    alert(`Error al Desconectar\n\n\t${err.message}`)
-    console.warn('Error al Desconectar',err);
+  catch (err) {
+    alert (`Error al Desconectar\n\n\t${err.message}`)
+    console.warn ('Error al Desconectar', err);
   }
   finally {
-    Utils.enableAll();
+    Utils.enableAll ();
   }
 }
