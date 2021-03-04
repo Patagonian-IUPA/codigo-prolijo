ef2556aeb0a21452fd0aa3664c9b74878b22f3ef
ElDev1 <49926868+ElDev1@users.noreply.github.com>
2021-03-04 13:47:59 -0300

    ejercicios codigo-prolijo
    ejercicios codigo-prolijo resueltos

--- class.js
+++ class.js
@@ -1,20 +1,19 @@
-class Foo
+class Foo 
 {
-color='negro';peso   =    34;
-  ver()
-  {
-    console.info(`Color: ${this.color}. Peso: ${this.peso}`
-    );
-// foo = 123;
-/*
-// Prueba
-test() {
-  console.info('Test');
+  color = "negro";
+  peso = 34;
+  ver() {
+    console.info(`Color: ${this.color}. Peso: ${this.peso}`);
+  }
 }
-*/
-}
-   }    class Bar   {
-  miMetodo(){if(this.imprimir){console.info('Imprimir!');}}
+
+class Bar 
+{
+  miMetodo() {
+    if (this.imprimir) {
+      console.info("Imprimir!");
+    }
+  }
 }
 
 // Agregar property "imprimir=true" para que "miMetodo()" se ejecute.
