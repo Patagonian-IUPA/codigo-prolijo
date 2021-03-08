dd2b19a57e61435d58c1c80b87bc39a70a088ab9
Martin Gonzalez <gonzalez.martin.mag@gmail.com>
2021-03-08 01:06:08 -0300

    Codigo formateado

--- class.js
+++ class.js
@@ -1,20 +1,17 @@
-class Foo
-{
-color='negro';peso   =    34;
-  ver()
-  {
-    console.info(`Color: ${this.color}. Peso: ${this.peso}`
-    );
-// foo = 123;
-/*
-// Prueba
-test() {
-  console.info('Test');
+class Foo {
+  color = 'negro';
+  peso = 34;
+  ver() {
+    console.info(`Color: ${this.color}. Peso: ${this.peso}`);
+  }
 }
-*/
-}
-   }    class Bar   {
-  miMetodo(){if(this.imprimir){console.info('Imprimir!');}}
+
+class Bar {
+  miMetodo() {
+    if (this.imprimir) {
+      console.info('Imprimir!');
+    }
+  }
 }
 
 // Agregar property "imprimir=true" para que "miMetodo()" se ejecute.
